77ba9bdc97f4f2e3c43000628fac94a53a82fda1
washington mugo <washiemugo@gmail.com>
2019-11-21 02:51:01 +0300

    designed the gawn collection form

--- gawn/pdf/pdf.php
+++ gawn/pdf/pdf.php
@@ -3,22 +3,25 @@
     
     $db = new PDO('mysql:host=localhost;dbname=dkut_clearance_system;','root','');
     //$reg = 'C025-02-0029/2015'; 
-
+    
     class myPDF extends FPDF{
         function header(){
+            $serial = 'Serial: 0010'.rand(123,999);
             $this->Image('logo.png',10,6);
             $this->SetFont('Arial','B',14);
-            $this->Cell(276,5,'DEPARTMENTS CLEARANCE FORM',0,0,'C');
+            $this->Cell(276,5,'GAWN COLLECTION FORM',0,0,'C');
             $this->Ln();
             $this->SetFont('Times','',12);
-            $this->Cell(276, 10, 'Student clearance form from all the necessary departments',0,0,'C');
+            $this->Cell(276, 10, 'Student Gawn Collection Form',0,0,'C');
+            $this->Ln();
+            $this->Cell(276, 10, $serial,0,0,'C');
             $this->Ln(40);
         }
         function footer(){
             $this->SetY(-26);
             $this->SetFont('Arial','',8);
             $this->Ln();
-            $this->Cell(0,10,'Dedan Kimathi University Clearance System',0,0,'C');
+            $this->Cell(0,10,'Dedan Kimathi University Clearance System [GOWN COLLECTION]',0,0,'C');
             $this->Ln();
             $this->Cell(0,10,'Page',0,0,'C');
         }
@@ -47,32 +50,41 @@
         function additioalinfo0(){
             // $this->Image('logo.png',10,6);
             $this->SetFont('Arial','B',14);
-            $this->Cell(276,5,'APPROVED DEPERTMENTS',0,0,'L');
+            $this->Cell(276,5,'GAWN COLLECTION PROGRESS ',0,0,'L');
             $this->Ln();
             $this->SetFont('Times','',12);
-            $this->Cell(276, 10, 'Student has officially been legally cleared th the following departments
-            and there is no additional information that need to be provide under his area',0,0,'L');
+            $this->Cell(276, 10, 'Follow the following procedures towards collecting and returning of the graduation gawn ',0,0,'L');
             $this->Ln(20);
         }
 
-        function viewDepartmentsheader(){
+        function progressheader(){
             $this->SetFont('Times','',12);
-            $this->Cell(30,10,'COD',1,0,'C');
-            $this->Cell(40,10,'librarian',1,0,'C');
-            $this->Cell(40,10,'Housekeeper',1,0,'C');
-            $this->Cell(50,10,'Sports Officer',1,0,'C');
-            $this->Cell(40,10,'Registrar',1,0,'C');
-            $this->Cell(40,10,'Finance',1,0,'C');
+            $this->Cell(30,10,'Progress',1,0,'C');
+            $this->Cell(40,10,'Date',1,0,'C');
+            $this->Cell(40,10,'time',1,0,'C');
+            $this->Cell(50,10,'Officer Sig.',1,0,'C');
+            $this->Cell(40,10,'Student Sig',1,0,'C');
+            $this->Cell(40,10,'Stamp',1,0,'C');
             $this->Ln();
         }
-        function viewDepartments(){
+        function viewcollection(){
             $this->SetFont('Times','',12);
-            $this->Cell(30,10,'Cleared',1,0,'C');
-            $this->Cell(40,10,'Cleared',1,0,'C');
-            $this->Cell(40,10,'Cleared',1,0,'C');
-            $this->Cell(50,10,'Cleared',1,0,'C');
-            $this->Cell(40,10,'Cleared',1,0,'C');
-            $this->Cell(40,10,'Cleared',1,0,'C');
+            $this->Cell(30,20,'Collection',1,0,'C');
+            $this->Cell(40,20,' ',1,0,'C');
+            $this->Cell(40,20,' ',1,0,'C');
+            $this->Cell(50,20,' ',1,0,'C');
+            $this->Cell(40,20,' ',1,0,'C');
+            $this->Cell(40,20,' ',1,0,'C');
+            $this->Ln();
+        }
+        function viewreturn(){
+            $this->SetFont('Times','',12);
+            $this->Cell(30,20,'Return',1,0,'C');
+            $this->Cell(40,20,' ',1,0,'C');
+            $this->Cell(40,20,' ',1,0,'C');
+            $this->Cell(50,20,' ',1,0,'C');
+            $this->Cell(40,20,' ',1,0,'C');
+            $this->Cell(40,20,' ',1,0,'C');
             $this->Ln(20);
         }
 
@@ -80,7 +92,7 @@
             $this->SetFont('Arial','B',14);
             $this->Cell(276,5,'Seal',0,0,'C');
             $this->getY();
-            $this->Image('cleared.png',110,130);
+            $this->Image('cleared.png',170,45);
             $this->Ln();
             $this->SetFont('Times','',12);
             $this->Ln(20);
@@ -93,8 +105,9 @@
     $pdf->headerTable();
     $pdf->viewUser($db);
     $pdf->additioalinfo0();
-    $pdf->viewDepartmentsheader();
-    $pdf->viewDepartments();
+    $pdf->progressheader();
+    $pdf->viewcollection();
+    $pdf->viewreturn();
     $pdf->additioalinfo2();
     $pdf->output();
 
